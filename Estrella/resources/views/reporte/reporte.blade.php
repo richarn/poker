551b0b8300afc9f336057c272959f34fc9b23d9a
richar <rnezferreira9@gmail.com>
2018-05-22 21:31:43 -0400

    add modal for some report views

--- Estrella/resources/views/reporte/reporte.blade.php
+++ Estrella/resources/views/reporte/reporte.blade.php
@@ -31,16 +31,171 @@
                             <div class="main_service_area">
                                 <div class="main_service_content">
                                     <div class="service_tabe">
-                                        <!-- Nav tabs -->                                   
+                                        <!-- Nav tabs -->   
 
+                                        <!--modalReporteBebida-->
+                                          <div class="modal fade" id="reporte_bebida" role="dialog">
+                                            <div class="modal-dialog">
+                                            
+                                              
+                                              <div class="modal-content">
+                                                <div class="modal-header">
+
+                                                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                  <h4 class="modal-title">Reporte de Bebidas y otros productos</h4>
+                                                
+                                                </div>
+                                                <div class="modal-body">
+
+                                             {{Form::open(array('url' => 'store/'))}}                  
+                                                    
+                                                      <div class="form-group form-inline">
+                                                
+                                                        <table class='table table-bordered table-striped table-hover table-responsive tbl-fecio' style="margin-top: 100px">
+                                                          
+                                                          <thead class="col-tab">
+                                                            <td>Nombre</td>
+                                                            <td>Cantidad</td>
+                                                            <td>Precio</td>
+                                                          </thead>
+                                                          <tbody>
+                                                          
+                                                            <tr>
+      
+                                                              <td></td>
+                                                              <td></td>
+                                                              <td></td>
+
+                                                            </tr> 
+                                                                                                                     
+                                                          </tbody>
+                                                        </table>
+                                                      </div><br/>
+                                                               
+                                                    
+                                                                                                    
+                                                </div>
+                                                <div class="modal-footer">
+                                                  <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                                                </div>
+                                                {{Form::close()}}
+                                              </div>
+                                              
+                                            </div>
+                                          </div>
+                                         <!--endmodalReporteBebida-->                                         
+
+                                        <!--modalReporteComida-->
+                                          <div class="modal fade" id="reporte_comida" role="dialog">
+                                            <div class="modal-dialog">
+                                            
+                                              
+                                              <div class="modal-content">
+                                                <div class="modal-header">
+
+                                                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                  <h4 class="modal-title">Reporte de Comestibles</h4>
+                                                
+                                                </div>
+                                                <div class="modal-body">
+
+                                             {{Form::open(array('url' => 'store/'))}}                  
+                                                    
+                                                      <div class="form-group form-inline">
+                                                
+                                                        <table class='table table-bordered table-striped table-hover table-responsive tbl-fecio' style="margin-top: 100px">
+                                                          
+                                                          <thead class="col-tab">
+                                                            <td>Nombre</td>
+                                                            <td>Cantidad</td>
+                                                            <td>Precio</td>
+                                                          </thead>
+                                                          <tbody>
+                                                          
+                                                            <tr>
+      
+                                                              <td></td>
+                                                              <td></td>
+                                                              <td></td>
+
+                                                            </tr> 
+                                                                                                                     
+                                                          </tbody>
+                                                        </table>
+                                                      </div><br/>
+                                                               
+                                                    
+                                                                                                    
+                                                </div>
+                                                <div class="modal-footer">
+                                                  <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                                                </div>
+                                                {{Form::close()}}
+                                              </div>
+                                              
+                                            </div>
+                                          </div>
+                                         <!--endmodalReporteComida-->                                                                                  
+
+                                        <!--modalReporteBillar-->
+                                          <div class="modal fade" id="reporte_billar" role="dialog">
+                                            <div class="modal-dialog">
+                                            
+                                              
+                                              <div class="modal-content">
+                                                <div class="modal-header">
+
+                                                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                  <h4 class="modal-title">Reporte de Billar</h4>
+                                                
+                                                </div>
+                                                <div class="modal-body">
+
+                                             {{Form::open(array('url' => 'store/'))}}                  
+                                                    
+                                                      <div class="form-group form-inline">
+                                                
+                                                        <table class='table table-bordered table-striped table-hover table-responsive tbl-fecio' style="margin-top: 100px">
+                                                          
+                                                          <thead class="col-tab">
+                                                            <td>Nombre</td>
+                                                            <td>Cantidad</td>
+                                                            <td>Precio</td>
+                                                          </thead>
+                                                          <tbody>
+                                                          
+                                                            <tr>
+      
+                                                              <td></td>
+                                                              <td></td>
+                                                              <td></td>
+
+                                                            </tr> 
+                                                                                                                     
+                                                          </tbody>
+                                                        </table>
+                                                      </div><br/>
+                                                               
+                                                    
+                                                                                                    
+                                                </div>
+                                                <div class="modal-footer">
+                                                  <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                                                </div>
+                                                {{Form::close()}}
+                                              </div>
+                                              
+                                            </div>
+                                          </div>
+                                         <!--endmodalReporteBillar-->                                                                                                                           
 
                                         <ul class="service_tabe_menu nav nav-tabs" role="tablist">
                                             
-                                            <a href="#" aria-controls="webdesign" role="tab"><li role="presentation" data-toggle="modal" data-target="#almacen_bebida"><i class="fa fa-map-marker"></i> <br />Bebidas</a></li>
+                                            <a href="#" aria-controls="webdesign" role="tab"><li role="presentation" data-toggle="modal" data-target="#reporte_bebida"><i class="fa fa-map-marker"></i> <br />Bebidas</a></li>
 
-                                            <a href="#" aria-controls="webdesign" role="tab"><li role="presentation" data-toggle="modal" data-target="#almacen_comida"><i class="fa fa-map-marker"></i> <br />Comestibles</a></li>
+                                            <a href="#" aria-controls="webdesign" role="tab"><li role="presentation" data-toggle="modal" data-target="#reporte_comida"><i class="fa fa-map-marker"></i> <br />Comestibles</a></li>
 
-                                            <a href="#" aria-controls="webdesign" role="tab"><li role="presentation" data-toggle="modal" data-target="#almacen_comida"><i class="fa fa-map-marker"></i> <br />Billar</a></li>
+                                            <a href="#" aria-controls="webdesign" role="tab"><li role="presentation" data-toggle="modal" data-target="#reporte_billar"><i class="fa fa-map-marker"></i> <br />Billar</a></li>
 
                                             <a href="#" aria-controls="webdesign" role="tab"><li role="presentation" data-toggle="modal" data-target="#almacen_comida"><i class="fa fa-map-marker"></i> <br />Pozo</a></li>
 
